c54f7bfa37dfddb850e5745c6cc6944fbb368481
Thea Harris <supershay@Theas-MacBook-Pro.local>
2020-12-28 22:49:23 -0500

    Commit

--- backend/controllers/businesses.js
+++ backend/controllers/businesses.js
@@ -22,11 +22,21 @@ businesses.delete('/:id', async (req, res) => {
     }
 })
 
-
 //UPDATE//
+businesses.put('/:id', async (req, res) => {
+    try {
+        const updatedBusiness = await Business.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+        res.status(200).json(updatedBusiness);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
 //CREATE//
-
 businesses.post('/', async (req,res) => {
     try{
         const createdBusiness = await Business.create(req.body);
@@ -37,6 +47,14 @@ businesses.post('/', async (req,res) => {
 });
 
 //SHOW//
+businesses.get("/:id", async(req,res) => {
+    try {
+        const showBusiness = await Business.findById(req.params.id,)
+        res.status(200).json(showBusiness);
+    } catch(err) {
+        res.status(400).json(err);
+    }
+})
 
 
 module.exports = businesses;
