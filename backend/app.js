c60e67fe41711417b38519fe6bf9d52fd05b4dd0
Thea Harris <supershay@Theas-MacBook-Pro.local>
2020-12-28 23:05:31 -0500

    Heroku

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const businessesController = require('./controllers/businesses.js');
