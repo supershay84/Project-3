e7d88ffd0c3b485535b9ac0a243aa608f8f8a868
Thea Harris <supershay@Theas-MacBook-Pro.local>
2021-01-03 21:34:44 -0500

    Authentication

--- backend/app.js
+++ backend/app.js
@@ -1,31 +1,20 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const businessesController = require('./controllers/businesses.js');
-const mongoose = require('mongoose');
 const MONGOURI = process.env.MONGODB_URI;
+const User = require('./models/user');
 
+const SECRET = 'inwestphiladelphiabornandraisedontheplaygroundiswhereispentmostofmydays'
 
 app.use(cors());
 app.use(express.json());
 
-// const whitelist = [
-//     'http://localhost:3007',
-//     'https://all-in-your-business.herokuapp.com/'
-//   ];
-//   const corsOptions = {
-//     origin: function (origin, callback) {
-//         if (whitelist.indexOf(origin) !== -1) {
-//             callback(null, true);
-//         } else {
-//             callback(new Error('Nah, son! Not allowed by CORS'));
-//         }
-//     },
-//   };
-  
- app.use(cors());
 
 mongoose.connect(MONGOURI, { useNewUrlParser: true, useUnifiedTopology: true, useFindandModify: true});
 
@@ -39,6 +28,37 @@ mongoose.connection.once('open', () => {
 });
 
 app.use('/businesses', businessesController);
+
+//REGISTER//
+app.post('/register', (req, res) => {
+    req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
+    User.create(req.body, (err, createdUser) => {
+        if(err){
+            res.status(400).json(err)
+        } else {
+            res.status(200).json(createdUser)
+        }
+    })
+})
+
+//LOGIN//
+app.post('/login', async (req, res) => {
+    const {username, password} = req.body;
+    try {
+        const user = await User.findOne({username})
+        if (bcrypt.compareSync(password, user.password)){
+            const token = jwt.sign({
+                username: user.username}, SECRET)
+            res.status(200).json({
+                token,
+                username,
+                authenticated: true
+            })
+        }
+    } catch(err) {
+        res.status(400).json(err)
+    }
+})
 
 //LISTEN//
 app.listen(PORT, () => {
--- backend/controllers/businesses.js
+++ backend/controllers/businesses.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const businesses = express.Router();
 const Business = require('../models/businesses.js');
+const jwt = require('jsonwebtoken');
+const SECRET = 'inwestphiladelphiabornandraisedontheplaygroundiswhereispentmostofmydays'
+
+const auth = async (req, res, next) => {
+    const {authorization} = req.headers;
+    if(authorization){
+        const token = authorization.split(' ')[1];
+        try{
+            const payload = await jwt.verify(token, SECRET);
+            req.user = payload;
+            next();
+        } catch(err) {
+            res.status(400).json(err)
+        }
+    } else {
+        res.status(400).json(new Error('No token in header'));
+    }
+}
 
 //INDEX//
 businesses.get('/', async (req,res) => {
@@ -13,7 +31,7 @@ businesses.get('/', async (req,res) => {
 });
 
 //DELETE//
-businesses.delete('/:id', async (req, res) => {
+businesses.delete('/:id', auth, async (req, res) => {
     try {
      const deletedBusiness = await Business.findByIdAndRemove(req.params.id);
      res.status(200).json(deletedBusiness);
@@ -39,17 +57,15 @@ businesses.put('/:id', async (req, res) => {
 //CREATE//
 businesses.post('/', async (req,res) => {
     try{
-        
         const createdBusiness = await Business.create(req.body);
         res.status(200).json(createdBusiness);
-        console.log(req.body)
     } catch(err) {
         res.status(400).json(err);
     }
 });
 
 //SHOW//
-businesses.get("/:id", async(req,res) => {
+businesses.get("/:id", async (req,res) => {
     try {
         const showBusiness = await Business.findById(req.params.id,)
         res.status(200).json(showBusiness);
